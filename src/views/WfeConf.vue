42050b945fa1a9f86160769c1e4c40ce7d173cc3
price_zh <zhang.ft@qq.com>
2018-08-28 09:09:04 +0800

    fix map component.

--- src/views/WfeConf.vue
+++ src/views/WfeConf.vue
@@ -25,32 +25,31 @@
       :lists = config.placeInfo.lists
     >
     </place-info>
-    <div class="row">
-      <div id="map" class="col-xs-8" style="margin:  0 auto;"></div>
+    <div class="row" style="margin-top: 30px;">
+      <div id="map"  style="margin:  0 auto;"></div>
     </div>
   </div>
 </template>
 
 
 <script>
-
-import config from '../configTxt'
-import Banner from '@/components/Banner'
-import ConfInfo from '@/components/ConfInfo'
-import Teacher from '@/components/Teacher'
-import TimeLine from '@/components/TimeLine'
-import ActivityShow from '@/components/ActivityShow'
-import PlaceInfo from '@/components/PlaceInfo'
-import LinkUs from '@/components/LinkUs'
+import config from "../configTxt";
+import Banner from "@/components/Banner";
+import ConfInfo from "@/components/ConfInfo";
+import Teacher from "@/components/Teacher";
+import TimeLine from "@/components/TimeLine";
+import ActivityShow from "@/components/ActivityShow";
+import PlaceInfo from "@/components/PlaceInfo";
+import LinkUs from "@/components/LinkUs";
 // import Map from '@/components/Map'
 export default {
-  name: 'WfeConf',
-  data () {
+  name: "WfeConf",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      msg: "Welcome to Your Vue.js App",
       config: config,
-      showAs: true,
-    }
+      showAs: true
+    };
   },
   components: {
     Banner,
@@ -59,40 +58,60 @@ export default {
     TimeLine,
     ActivityShow,
     LinkUs,
-    PlaceInfo,
+    PlaceInfo
     // Map
   },
   mounted() {
-    this.ready()
+    this.ready();
   },
   methods: {
     ready: function() {
-      var map = new BMap.Map('map')
-      var point = new BMap.Point(108.840053, 34.129522)
-      map.centerAndZoom(point, 14)
-      map.addControl(new BMap.MapTypeControl())
-      map.enableScrollWheelZoom(true)
-      map.enableDoubleClickZoom(true)
-      var marker = new BMap.Marker(point)
-      map.addOverlay(marker)
-      map.centerAndZoom(point, 15)
+      var map = new BMap.Map("map");
+      var point = new BMap.Point(108.840053, 34.129522);
+      map.centerAndZoom(point, 14);
+      // map.addControl(new BMap.MapTypeControl());
+      map.enableScrollWheelZoom(true);
+      map.enableDoubleClickZoom(true);
+      var marker = new BMap.Marker(point);
+      map.addOverlay(marker);
+      var opts = {
+        width: 200, 
+        height: 100, 
+        title: "海底捞王府井店", 
+        enableMessage: true, 
+        offset:{
+          width: 0,
+          height: -15
+
+        },
+        message: "亲耐滴，晚上一起吃个饭吧？戳下面的链接看下地址喔~"
+      };
+      var infoWindow = new BMap.InfoWindow(
+        "地址：北京市东城区王府井大街88号乐天银泰百货八层",
+        opts
+      );
+      map.centerAndZoom(point, 15);
+      map.openInfoWindow(infoWindow, point);
     }
   }
-}
+};
 </script>
 
 
 <style scoped>
 #map {
   /* width: 600px; */
+  width: 55%;
   height: 600px;
-  margin:  0 auto;
+  margin: 0 auto;
 }
 
 @media (max-width: 768px) {
   #map {
     /* width: 350px; */
+    width: 70%;
     height: 350px;
+
   }
 }
 </style>
