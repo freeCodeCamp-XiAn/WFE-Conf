17f2f186e39b20af51d078147b518550861ec22d
hylerrix <hylerrix@foxmail.com>
2018-08-28 13:01:03 +0800

    修改通用文本信息，添加 level 字段表示分享难度等级

--- src/configTxt.js
+++ src/configTxt.js
@@ -8,7 +8,7 @@ const config = {
   confInfo: {
     titlezh: '大会简介',
     titlen: 'INTRODUCTION',
-    intro: 'FreeCodeCamp 成都社区一直以为成都技术爱好者以及用户提供一个开放的交流环境为目的，在过去的一年里，我们成功举办了30余次的社区活动，受到大家的一致好评和热烈响应。2017年已接近尾声，在组织部何青松的倡议下，我们特筹办freeCodeCamp【2017成都WEB前端交流大会】，为成都前端圈的技术工程师、爱好者提供一场前所未有的饕餮盛宴。在此，我们向您发出诚挚的邀请，此次盛会期待您的参与！',
+    intro: 'FCC 西安社区一直以为西安本土开发者提供一个开放交流的环境为目的，成功举办了十余次社区活动，受到大家的广泛好评和热烈响应。这次正逢 FCC 西安社区诞生两周年，在持续酝酿下，我们特地于两周年当日筹办这场【2018 西安 Web 前端交流大会】，为西安本土的前端工程师、开源爱好者及互联网从业人员们提供一场前所未有的技术盛宴。此次盛会期待您的参与！',
   },
   teacher: {
     config: {
@@ -20,37 +20,43 @@ const config = {
         id: '1',
         path: '../../static/teacher/huikun.png',
         name: '惠坤',
-        intro: 'ThoughtWorks 咨询师，一直致力于研究如何将更好的用户体验带到前端。'
+        intro: 'ThoughtWorks 咨询师，一直致力于研究如何将更好的用户体验带到前端。',
+        level: '中级'
       },
       {
         id: '2',
         path: '../../static/teacher/wangyuanbo.png',
         name: '王渊博',
-        intro: '易宝，背锅抗雷前端一枚。'
+        intro: '易宝，背锅抗雷前端一枚。',
+        level: '初级'
       },
       {
         id: '3',
         path: '../../static/teacher/leili.png',
         name: '雷力',
-        intro: 'Node 区块链工程师、全栈开发工程师，目前关注形式化验证与智能合约安全。'
+        intro: 'Node 区块链工程师、全栈开发工程师，目前关注形式化验证与智能合约安全。',
+        level: '中级'
       },
       {
         id: '4',
         path: '../../static/teacher/hanyile.png',
         name: '韩亦乐',
-        intro: 'freeCodeCamp 2018 Top Contributor，开源爱好者，参与、组织过多次开源技术社区活动，目前在滴滴进行前端岗位的实习。'
+        intro: 'freeCodeCamp 2018 Top Contributor，开源爱好者，参与、组织过多次开源技术社区活动，目前在滴滴进行前端岗位的实习。',
+        level: '中级'
       },
       {
         id: '5',
         path: '../../static/teacher/muyang.png',
         name: '慕阳',
-        intro: '华为云 devcloud 前端工程师，曾供职于美团网、人人网。目前在华为云 devckoud 负责前端架构、性能优化与公共服务建设。'
+        intro: '华为云 devcloud 前端工程师，曾供职于美团网、人人网。目前在华为云 devckoud 负责前端架构、性能优化与公共服务建设。',
+        level: '高级'
       },
       {
         id: '6',
         path: '../../static/teacher/liyaodong.png',
         name: '李耀东',
-        intro: '曾在极客公园做前端，现 ThoughtWorks 工程师，正在学习网站性能优化中。'
+        intro: '曾在极客公园做前端，现 ThoughtWorks 工程师，正在学习网站性能优化中。',
+        level: '高级'
       }
     ]
   },
@@ -78,7 +84,7 @@ const config = {
         time: '10:20-11:10',
         title: '慧坤 - 揭秘贝塞尔曲线在 Web 中的应用',
         isSpeech: true,
-        type: '备注',
+        type: '主题描述',
         info: '。'
       },
       {
@@ -86,7 +92,7 @@ const config = {
         time: '11:10-12:00',
         title: '王渊博 - Typescript 介绍',
         isSpeech: true,
-        type: '备注',
+        type: '主题描述',
         info: '。'
       },
       {
@@ -102,7 +108,7 @@ const config = {
         time: '13:00-13:50',
         title: '雷力 - 基于以太坊的 DApp 开发实践',
         isSpeech: true,
-        type: '备注',
+        type: '主题描述',
         info: '。'
       },
       {
@@ -110,7 +116,7 @@ const config = {
         time: '13:50-14:40',
         title: '韩亦乐 - 浅尝初试微前端',
         isSpeech: true,
-        type: '备注',
+        type: '主题描述',
         info: '。'
       },
       {
@@ -126,7 +132,7 @@ const config = {
         time: '15:20-16:10',
         title: '慕阳 - 基于 ng 的前端微服务实践',
         isSpeech: true,
-        type: '备注',
+        type: '主题描述',
         info: '。'
       },
       {
@@ -134,7 +140,7 @@ const config = {
         time: '16:10-17:00',
         title: '李耀东 - Building blazing fast website with AMP',
         isSpeech: true,
-        type: '备注',
+        type: '主题描述',
         info: '。'
       },
       {
@@ -142,7 +148,7 @@ const config = {
         time: '17:00-17:30',
         title: '交流+颁奖+抽奖',
         isSpeech: false,
-        type: '备注',
+        type: '主题描述',
         info: '。'
       }
     ],
@@ -196,6 +202,6 @@ const config = {
     path1: '../../static/qrcode/qrcode.jpg',
     path2: '../../static/qrcode/qrcode.jpg'
   }
-
 }
+
 export default config ;
